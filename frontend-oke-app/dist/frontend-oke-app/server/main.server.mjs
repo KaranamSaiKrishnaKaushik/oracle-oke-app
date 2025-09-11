a5acb8d58d28a54dc87563a9cd1fb264709c4004
KaranamK <kaushik.karanam@l-p-a.com>
2025-09-11 05:26:59 +0200

    angular.json fileReplacement

--- frontend-oke-app/dist/frontend-oke-app/server/main.server.mjs
+++ frontend-oke-app/dist/frontend-oke-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NSXHAX2L.mjs";import"./chunk-SWEZUDQX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EFJI7SC5.mjs";import"./chunk-SWEZUDQX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
